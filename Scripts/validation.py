8eba368a18c92ac593de54c3836d7cd321fd90ca
YuJheWang <turtlejaywang@gmail.com>
2025-01-20 22:31:30 +0800

    remove wierd import

--- Scripts/validation.py
+++ Scripts/validation.py
@@ -17,40 +17,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 from skimage import measure
-from skimage.draw import ellipsoid
-
-def create_marching_cube_mesh(voxel_grid : np.ndarray, network : FullNetwork, config : config.Config):
-    x = torch.linspace(0, 1, 100)
-    y = torch.linspace(0, 1, 100)
-    z = torch.linspace(0, 1, 100)
-    x, y, z = torch.meshgrid(x, y, z, indexing='ij')
-    points = torch.stack([x.flatten(), y.flatten(), z.flatten()], dim=1)
-    point_batch = torch.split(points, 200000)
-
-    latent_code = network.encoder(torch.from_numpy(voxel_grid))
-    sdfs = torch.zeros(0)
-    for points in point_batch:
-        points = points.to(device=config.device)
-        latent_code_repeated = repeat(latent_code, "l -> s l", s=points.shape[0])
-        sdfs = torch.cat((sdfs, network.decoder(latent_code_repeated, points)))
-    sdfs.flatten()
-    
-    sdfs = sdfs.cpu().numpy().reshape(100, 100, 100)
-    verts, faces, normals, values = measure.marching_cubes(sdfs, level=0)
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-    mesh = Poly3DCollection(verts[faces])
-    mesh.set_edgecolor('k')
-    ax.add_collection3d(mesh)
-    
-    ax.set_xlim(0, sdfs.shape[0])
-    ax.set_ylim(0, sdfs.shape[1])
-    ax.set_zlim(0, sdfs.shape[2])
-    
-    plt.show()
-
 
 def validation(network : FullNetwork, validation_loader : DataLoader, config : config.Config):
     network.eval()
